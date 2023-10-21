5a8adcae9756be18748744eda6b2c6b103d85370
Ronie <ronielvb@hotmail.com>
2023-10-21 19:17:47 -0300

    Sessão flask

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+#HOMOLOGAÇÃO - Endereço para validar o token do link
+end_point_sgr="https://homsgrapi.azurewebsites.net/api/validar-token/"
+
 @app.route('/')
 def index():
   if not session.get("nome"):
@@ -14,10 +17,11 @@ def index():
   
   return redirect("/dashboard")
 
+# Autentica usuário no python via sessão do flask
 @app.route('/auth')
 def auth():
   token = request.args['token']
-  response = requests.get("https://homsgrapi.azurewebsites.net/api/validar-token/" + token)
+  response = requests.get(end_point_sgr + token)
 
   if response.status_code == 200:
     json_data = response.json()
